21ef5b24f646eb2ced4fe92a23eda92c18ea3c18
Utsav Manandhar <utsavmanandharz@gmail.com>
2023-08-04 14:58:35 +0545

    added some comments on what to do

--- HEAD/apps/students/models.py
+++ HEAD/apps/students/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.conf import settings
 
-
+User = settings.AUTH_USER_MODEL
 # Create your models here.
 class ProfessorData(models.Model):
     name = models.CharField(max_length=24, null=True)
@@ -21,8 +22,15 @@ class StatusData(models.Model):
         return self.name
 
 class StudentData(models.Model):
-   
-		
+    # something like this should be added in models
+    # but since we're adding to already existing data some default value needs to be given 
+    # This is a nightmare lmao 
+    # student = models.ForeignKey(
+	# 	User,
+	# 	on_delete=models.CASCADE,
+	# 	related_name="my_requests",
+	# 	related_query_name="my_requests"
+	# )	
     name = models.CharField(max_length=64, unique=True)
     university = models.ForeignKey(UniversityData, on_delete=models.SET_NULL, null=True)
     professor = models.ForeignKey(ProfessorData,on_delete=models.SET_NULL,null=True)
--- HEAD/views.py
+++ HEAD/views.py
@@ -5,22 +5,38 @@ from django.views.generic.base import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from HEAD.apps.students.models import StudentData
 from .forms import UpdateForm
+from django.contrib.auth import login
 
 def update_status(request):
-    # this page should only be accessed by a student so make check here
+    
     if request.user.is_authenticated:
+        # this page should only be accessed by a student so make check here
         current_user = StudentData.objects.get(id = 2)
-        print(request.user.id)
+        #print(request.user.id)
        
-        #right now im getting user with id 1 by default but you should get the id of
-        #the student that is logged in
+        # right now im getting user with id 1 by default but you should get the id of
+        # the student that is logged in
+
         # current_user = StudentData.objects.get(id = request.user.id)
+
         # but that data is fucked because your StudentData model is independent from the users
-        # so you need to add the user to the model itself
+        # so you need to find out which user is logged in 
+        # ( which would have been easy if the StudentData model was already related to the Users)
+        # check models for that ive added an example
+        # but i guess you have to do code in some loop check to check which user is currently logged in
+        # and match that student to their status
+         
         form = UpdateForm(request.POST or None, instance=current_user)
         if form.is_valid():
             form.save()
-        return render(request, 'blank.html', {'form':form})
+            return render(request, 'blank.html', {'form':form})
+        # make this go back to the users profile page or somewhere else
+        # doing that might log out the user so add this line after form.save()
+        # login(request, current_user)
+        # login needs to be imported (check top)
+        else:
+            return render(request, 'blank.html', {'form':form})
+        # show an error message saying form is invalid
 
 
 
