e2141cd34e95c768b8179ba96252d758382b33e7
Utsav Manandhar <utsavmanandharz@gmail.com>
2023-08-06 21:50:45 +0545

    added new status moddel

--- HEAD/apps/students/admin.py
+++ HEAD/apps/students/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import StudentData,ProfessorData,StatusData,UniversityData
+from .models import StudentData,ProfessorData,StatusData,UniversityData,studentStatus
 # Register your models here.
 admin.site.register(StudentData)
 admin.site.register(ProfessorData)
 admin.site.register(UniversityData)
 admin.site.register(StatusData)
+admin.site.register(studentStatus)
--- HEAD/apps/students/models.py
+++ HEAD/apps/students/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 
 # Create your models here.
@@ -26,6 +28,28 @@ class StatusData(models.Model):
 
 class StudentData(models.Model):
     name = models.CharField(max_length=64, unique=True)
+    university = models.ForeignKey(
+        UniversityData, on_delete=models.SET_NULL, blank=True, null=True
+    )
+    professor = models.ForeignKey(
+        ProfessorData, on_delete=models.SET_NULL, blank=True, null=True
+    )
+    age = models.IntegerField(
+        default=0, validators=[MaxValueValidator(100), MinValueValidator(1)], blank=True
+    )
+    date = models.DateField(blank=True, null=True)
+    status = models.ForeignKey(
+        StatusData, on_delete=models.SET_NULL, blank=True, null=True
+    )
+
+    def __str__(self):
+        return self.name
+    
+class studentStatus(models.Model):
+    student = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
     university = models.ForeignKey(
         UniversityData, on_delete=models.SET_NULL, blank=True, null=True
     )
