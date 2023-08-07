00b795dc35a6a9553114376a72db54d9dee00737
RohanM <rohan13maharjan@gmail.com>
2023-08-07 18:40:51 +0545

    Changes review to integrate uttu's add user feature

--- HEAD/forms.py
+++ HEAD/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.forms import ModelForm
-from HEAD.apps.students.models import StudentData,studentStatus
+from HEAD.apps.students.models import StudentData
 from django.contrib.auth.models import User, Group
 
 #Create a student form
@@ -8,11 +8,6 @@ class UpdateForm(ModelForm):
     class Meta:
         model = StudentData
         fields = ['name', 'university','date','status']
-
-class UpdateStatusForm(ModelForm):
-    class Meta:
-        model = studentStatus
-        fields = ['university','date','status']
 
 
 class AddNewUserForm(forms.ModelForm):
