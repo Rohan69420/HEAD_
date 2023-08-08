e012bc88cd73fa36d294decacca3cb191164c173
RohanM <rohan13maharjan@gmail.com>
2023-08-08 18:40:10 +0545

    Removed apps app

--- HEAD/apps/students/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import StudentData,ProfessorData,StatusData,UniversityData
-# Register your models here.
-admin.site.register(StudentData)
-admin.site.register(ProfessorData)
-admin.site.register(UniversityData)
-admin.site.register(StatusData)
--- HEAD/apps/students/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class StudentsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'students'
-    default = False
