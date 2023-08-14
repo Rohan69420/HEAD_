520ce4b3f7eeaf6e358475563face7ea6d1d2d7a
RohanM <rohan13maharjan@gmail.com>
2023-08-14 20:24:15 +0545

    Static file location changed

--- HEAD/settings.py
+++ HEAD/settings.py
@@ -57,7 +57,7 @@ TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [
-            os.path.join(BASE_DIR, "static/theme"),
+            os.path.join(BASE_DIR, "static"),
         ],
         "APP_DIRS": True,
         "OPTIONS": {
@@ -119,8 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/theme/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static/theme/")]
+STATIC_URL = "static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static/")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
