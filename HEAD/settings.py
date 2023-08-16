236085e97573474b937e1ddfd97ab73b6e3d30d6
Utsav Manandhar <utsavmanandharz@gmail.com>
2023-08-16 20:05:27 +0545

    switched to python decoupler

--- HEAD/settings.py
+++ HEAD/settings.py
@@ -12,9 +12,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 # loading environment variables
 import os
 from pathlib import Path
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
+from decouple import config
 
 
 
@@ -94,11 +95,11 @@ WSGI_APPLICATION = "HEAD.wsgi.application"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get("DB_NAME"),
-        'USER': os.environ.get("DB_USER"),
-        'PASSWORD': os.environ.get("DB_PASSWORD"),
-        'HOST': os.environ.get("DB_HOST"),
-        'PORT': os.environ.get("DB_PORT"),
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT':config('DB_PORT')
     }
 }
 
