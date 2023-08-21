925ec1f7be9b207da2eca7008f5dd3b3fa6bde7e
Utsav Manandhar <utsavmanandharz@gmail.com>
2023-08-21 19:04:59 +0545

    ugh fine i will use docker

--- HEAD/settings.py
+++ HEAD/settings.py
@@ -58,7 +58,9 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
-CSRF_TRUSTED_ORIGINS = ['*']
+
+
+
 ROOT_URLCONF = "HEAD.urls"
 
 TEMPLATES = [
